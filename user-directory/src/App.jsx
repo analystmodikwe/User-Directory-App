d342508946251e0af7882a8219c30b28b7d07d35
lesedi <analystmodikwe18@gmail.com>
2026-07-14 03:18:23 +0200

    Testing the useUser.js

--- user-directory/src/App.jsx
+++ user-directory/src/App.jsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
+// import { useState } from 'react';
 
 
-function App() {
- 
-    
-  
+
+// function App() {
+//    return(
+//     <div>
+//       <useUsers />
+//     </div>
+//    )
+// }
+
+// export default App
+
+// testing the API
+import { useUsers } from "./hooks/useUsers.js";
+
+export default function App() {
+  const { users, isLoading, error } = useUsers();
+
+  // Temporary — just to prove the fetch works.
+  console.log("users:", users);
+  console.log("isLoading:", isLoading);
+  console.log("error:", error);
+
+  return <h1>Check the console</h1>;
 }
-
-export default App
